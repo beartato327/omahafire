da1cebfe88cf9c237abc6ae69879b8701e11441e
beartato327 <rcapurro@unomaha.edu>
2019-10-11 16:10:26 +0000

    permission levels added

--- home/wagtail_hooks.py
+++
@@ -1,6 +0,0 @@
-from wagtail.core import hooks
-from django.contrib.auth.models import Permission
-
-@hooks.register('register_permissions')
-def view_restricted_page():
-    return Permission.objects.filter(codename="view_restricted_document")
--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    #add attributes here
     pass
     
     def __str__(self):
